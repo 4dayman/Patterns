7497973bb4ee67b816f9a0debe7ca24bf3b57d85
4dayman <109858025+4dayman@users.noreply.github.com>
2022-11-29 12:34:45 +0200

    commit

--- Structural (Структурные)/Composite (Компоновщик)/composite.js
+++ Structural (Структурные)/Composite (Компоновщик)/composite.js
@@ -1,69 +1,91 @@
-var Developer = /** @class */ (function () {
-    function Developer(name, salary) {
-        this.name = name;
-        this.role = 'Developer';
-        if (salary) {
-            this.salary = salary;
-        }
-        else {
-            this.salary = 1000;
-        }
-    }
-    Developer.prototype.getName = function () {
-        return this.name;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    Developer.prototype.getSalary = function () {
-        return this.salary;
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    Developer.prototype.getRole = function () {
-        return this.role;
-    };
-    return Developer;
-}());
-var Designer = /** @class */ (function () {
-    function Designer(name, salary) {
-        this.name = name;
-        this.role = 'Designer';
-        if (salary) {
-            this.salary = salary;
-        }
-        else {
-            this.salary = 1000;
-        }
-    }
-    Designer.prototype.getName = function () {
-        return this.name;
-    };
-    Designer.prototype.getSalary = function () {
-        return this.salary;
-    };
-    Designer.prototype.getRole = function () {
-        return this.role;
-    };
-    return Designer;
-}());
+})();
 var Company = /** @class */ (function () {
     function Company() {
-        this.employees = [];
     }
-    Company.prototype.addEmployee = function (employee) {
-        this.employees.push(employee);
+    Company.prototype.setParent = function (parent) {
+        this.parent = parent;
     };
-    Company.prototype.removeEmployee = function (employee) {
-        this.employees.slice(1);
+    Company.prototype.getParent = function () {
+        return this.parent;
     };
-    Company.prototype.salaryEmployee = function () {
-        this.employees.forEach(function (employee) {
-            console.log('Name: ' + employee.getName() + ' Salary: ' + employee.getSalary());
-        });
-    };
+    Company.prototype.add = function (company) { };
+    Company.prototype.remove = function (company) { };
     return Company;
 }());
-var company = new Company();
-company.addEmployee(new Designer('Steve;', 2000));
-company.addEmployee(new Designer('Steven;'));
-company.addEmployee(new Developer('Stevey;', 3000));
-company.addEmployee(new Developer('Steveo;'));
-console.log('Company staff:');
-company.salaryEmployee();
-// console.log(this.result);
+var Designer = /** @class */ (function (_super) {
+    __extends(Designer, _super);
+    function Designer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Designer.prototype.stuff = function () {
+        return 'Designer';
+    };
+    return Designer;
+}(Company));
+var Developer = /** @class */ (function (_super) {
+    __extends(Developer, _super);
+    function Developer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Developer.prototype.stuff = function () {
+        return 'Developer';
+    };
+    return Developer;
+}(Company));
+var Department = /** @class */ (function (_super) {
+    __extends(Department, _super);
+    function Department() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.children = new Array();
+        return _this;
+    }
+    Department.prototype.add = function (company) {
+        this.children.push(company);
+        company.setParent(this);
+    };
+    Department.prototype.remove = function (company) {
+        var companytIndex = this.children.indexOf(company);
+        this.children.splice(companytIndex, 1);
+        company.setParent(null);
+    };
+    Department.prototype.stuff = function () {
+        var array = [];
+        for (var _i = 0, _a = this.children; _i < _a.length; _i++) {
+            var child = _a[_i];
+            array.push(child.stuff());
+        }
+        return " Company Department of \"".concat(array.join(' and '), "\"");
+    };
+    return Department;
+}(Company));
+function companyEmployees(company) {
+    console.log("Company Employees: ".concat(company.stuff()));
+}
+var company = new Department();
+var department = new Department();
+department.add(new Designer());
+department.add(new Developer());
+company.add(department);
+var otherDepartment = new Department();
+otherDepartment.add(new Developer());
+otherDepartment.add(new Designer());
+company.add(otherDepartment);
+var anotherDepartment = new Department();
+anotherDepartment.add(new Developer());
+anotherDepartment.add(new Designer());
+company.add(anotherDepartment);
+companyEmployees(company);
--- Structural (Структурные)/Composite (Компоновщик)/composite.ts
+++ Structural (Структурные)/Composite (Компоновщик)/composite.ts
@@ -1,76 +1,67 @@
-interface Employee {
-    getName(): string;
-    getSalary(): number;
-    getRole(): string;
+interface CompanyInterface {
+    stuff(): string;
 }
-class Developer implements Employee {
-    name: string;
-    salary: number;
-    role: string;
-    constructor(name: string, salary?: number){
-        this.name = name;
-        this.role = 'Developer';
-        if(salary){
-            this.salary = salary;
-        } else {
-            this.salary = 1000;
+
+abstract class Company implements CompanyInterface{
+    parent!: Company | null;
+    setParent(parent: Company | null) {
+        this.parent = parent;
+    }
+    getParent(): Company | null {
+        return this.parent;
+    }
+    add(company: Company): void {}
+    remove(company: Company): void {}
+    abstract stuff(): string;
+}
+
+class Designer extends Company {
+    stuff(): string {
+        return 'Designer';
+    }
+}
+
+class Developer extends Company {
+    stuff(): string {
+        return 'Developer';
+    }
+}
+
+class Department extends Company {
+    children: Array<Company> = new Array<Company>();
+    add(company: Company): void {
+        this.children.push(company);
+        company.setParent(this);
+    }
+    remove(company: Company): void {
+        const companytIndex = this.children.indexOf(company);
+        this.children.splice(companytIndex, 1);
+        company.setParent(null);
+    }
+    stuff() {
+        const array = [];
+        for (const child of this.children) {
+            array.push(child.stuff());
         }
-    }
-    getName(): string {
-        return this.name;
-    }
-    getSalary(): number {
-        return this.salary;
-    }
-    getRole(): string {
-        return this.role;
+        return ` Company Department of \"${array.join(' and ')}\"`;
     }
 }
-class Designer implements Employee {
-    name: string;
-    salary: number;
-    role: string;
-    constructor(name: string, salary?: number){
-        this.name = name;
-        this.role = 'Designer';
-        if(salary){
-            this.salary = salary;
-        } else {
-            this.salary = 1000;
-        }
-    }
-    getName(): string {
-        return this.name;
-    }
-    getSalary(): number {
-        return this.salary;
-    }
-    getRole(): string {
-        return this.role;
-    }
+
+function companyEmployees(company: Company) {
+    console.log(`Company Employees: ${company.stuff()}`);
 }
-class Company {
-    employees: Array<Employee>;
-    constructor() {
-        this.employees = [];
-    }
-    addEmployee(employee: Employee): void{
-        this.employees.push(employee);
-    }
-    removeEmployee(employee: Employee): void{
-        this.employees.slice(1)
-    }
-    salaryEmployee(): void{
-        this.employees.forEach( function (employee) {
-            console.log('Name: ' + employee.getName() + ' Salary: ' + employee.getSalary());
-        })
-    }
-}
-const company = new Company();
-company.addEmployee(new Designer('Steve;', 2000));
-company.addEmployee(new Designer('Steven;'));
-company.addEmployee(new Developer('Stevey;', 3000));
-company.addEmployee(new Developer('Steveo;'));
-console.log('Company staff:');
-company.salaryEmployee();
-// console.log(this.result);
+
+const company = new Department();
+const department = new Department();
+department.add(new Designer());
+department.add(new Developer());
+company.add(department);
+const otherDepartment = new Department();
+otherDepartment.add(new Developer());
+otherDepartment.add(new Designer());
+company.add(otherDepartment);
+const anotherDepartment = new Department();
+anotherDepartment.add(new Developer());
+anotherDepartment.add(new Designer());
+company.add(anotherDepartment);
+companyEmployees(company);
